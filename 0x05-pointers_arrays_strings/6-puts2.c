271b4d1abe14fc07fd6947b0fe7523dd5513dfdc
Baabalwaaiz <baabalwaaiz@gmail.com>
2022-04-20 20:31:59 -0700

    print every other character

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * puts2 - Pints every other character of a string.
+ * @str: String for alternate printing
+ * Return: Always 0.
+ */
+void puts2(char *str)
+{
+	int i, j;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+
+	i--;
+
+	for (j = 0; j <= i; j++)
+	{
+		if (j % 2 == 0)
+			_putchar(str[j]);
+	}
+
+	_putchar('\n');
+}
+
+
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -2,3 +2,4 @@ Pointer restes value of variable
 sap_int: swap values of two integers
 Return length of string
 Print string in reverse
+Print every other character of a string
