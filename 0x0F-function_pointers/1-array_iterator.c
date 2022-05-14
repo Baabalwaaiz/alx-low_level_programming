72a2fd159e1f7249ccd69139e58b426479771c67
Baabalwaaiz <baabalwaaiz@gmail.com>
2022-05-13 23:40:42 -0700

    prints all array content

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,6 +1,5 @@
 #include "function_pointers.h"
 #include <stdio.h>
-#include <stddef.h>
 
 /**
  * array_iterator - execute function on an array using a function ptr
@@ -11,9 +10,16 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	size_t i; /* match data type */
+	unsigned int i;
+	void (*ptr)(int);
 
-	if (array != NULL && action != NULL)
-		for (i = 0; i < size; i++)
-			action(*(array + i));
+	if (size <= 0)
+		return;
+
+	if (array == NULL || action == NULL)
+		return;
+
+	ptr = action;
+	for (i = 0; i < size; i++)
+		ptr(array[i]);
 }
