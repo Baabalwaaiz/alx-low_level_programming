8e0045efac567a32da8b27cf62c5961360e060cc
Baabalwaaiz <baabalwaaiz@gmail.com>
2022-05-17 22:21:06 -0700

    added new header

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,25 +1,10 @@
 #ifndef VARIADIC_FUNCTIONS_H
-#define VARIADIC-FUNCTIONS_H
-
-#include <stdarg.h>
+#define VARIADIC_FUNCTIONS_H
 
 int _putchar(char c);
-int sum_them_all(const unsigned int n, ...);
+int sum_them_all(const unsigned int, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-
-/**
- * struct printer - A new struct type defining a printer.
- * @symbol: A symbol representing a data type.
- * @print: A function pointer to a function that prints
- *         a data type corresponding to symbol.
- */
-typedef struct printer
-{
-	char *symbol;
-	void (*print)(va_list arg);
-
-} printer_t;
 
 #endif
